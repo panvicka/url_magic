6856ca36231c9e13bdb27a32377e5ef214ea8610
Eliska Veisova <eliska.veisova@bitgrip.de>
2024-11-11 19:47:44 +0100

    test

--- apps/cm-url-magic-web/src/hooks.server.ts
+++ apps/cm-url-magic-web/src/hooks.server.ts
@@ -1,24 +1,26 @@
-import { BASE_AUTH_ADMIN_LOGIN } from '$env/static/private';
-import type { Handle } from '@sveltejs/kit';
+import { env } from "$env/dynamic/private";
+
+import type { Handle } from "@sveltejs/kit";
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({
-	event,
-	resolve
+  event,
+  resolve,
 }: Parameters<Handle>[0]): Promise<ReturnType<Handle>> {
-	const url = new URL(event.request.url);
+  const url = new URL(event.request.url);
 
-	if (url.pathname.startsWith('/') && !url.pathname.startsWith('/api')) {
-		const auth = event.request.headers.get('Authorization');
-		if (auth !== `Basic ${btoa(BASE_AUTH_ADMIN_LOGIN)}`) {
-			return new Response('Not authorized', {
-				status: 401,
-				headers: {
-					'WWW-Authenticate': 'Basic realm="User Visible Realm", charset="UTF-8"'
-				}
-			});
-		}
-	}
+  if (url.pathname.startsWith("/") && !url.pathname.startsWith("/api")) {
+    const auth = event.request.headers.get("Authorization");
+    if (auth !== `Basic ${btoa(env.BASE_AUTH_ADMIN_LOGIN)}`) {
+      return new Response("Not authorized", {
+        status: 401,
+        headers: {
+          "WWW-Authenticate":
+            'Basic realm="User Visible Realm", charset="UTF-8"',
+        },
+      });
+    }
+  }
 
-	return resolve(event);
+  return resolve(event);
 }
